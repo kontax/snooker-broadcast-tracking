14cca48e6e804cbfe21b22b33e2ad3e5ef0a4d4d
James Coulson <coulsonj@gmail.com>
2016-02-21 23:06:09 +0000

    Adding the data extraction scripts and folder

---
+++ data/extract_images.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python2
+# Extracts a selection of random images from the downloaded videos and saves them to a folder
+
+import sys
+import getopt
+import random
+import imageio
+from os import listdir
+from os.path import isfile, join, basename, dirname
+
+def main(argv):
+
+    arg = argv[1]
+    video = basename(arg)
+    directory_name = dirname(arg) + '/'
+
+
+    random.seed(10)
+    images_to_extract = 50
+    #directory_name = '../../data/short_videos/fixed/'
+    video_extension = '.mp4'
+    #files = [f for f in listdir(directory_name) if isfile(join(directory_name, f))]
+
+    #for video in files:
+
+    filename = video[:5]
+    full_filename = directory_name + video
+    with imageio.get_reader(full_filename, 'ffmpeg') as vid:
+        frame_count = vid.get_meta_data()['nframes']
+
+        for i in range(0,images_to_extract):
+            random_number = random.randint(0, frame_count)
+            #print random_number
+            image = vid.get_data(random_number-1)
+            image_filename = filename + '-' + '{0:02d}'.format(i) + '.jpg'
+            imageio.imwrite(directory_name + '../images/' + image_filename, image)
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ data/get_data.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python2
+# A script which downloads the selected list of files from YouTube, then converts them into the necessary
+# formats for use in training the networks within the project.
+#
+# Note: YouTube-DL needs to be downloaded and installed. It can be found at:
+#           https://github.com/rg3/youtube-dl
+#
+#       and installed with the following command on a linux machine:
+#           git clone https://github.com/rg3/youtube-dl &&
+#           cd youtube-dl &&
+#           python2 setup.py install
+
+from __future__ import unicode_literals
+import youtube_dl
+
+def main():
+    # Get the save location and URL's for the youtube video's to download
+    #video_location = '../../data/videos/'
+    video_location = '../../data/short_videos/'
+    youtube_url_file_location = 'short_videos.txt'
+    ydl_opts = get_youtube_dl_options("short", video_location)
+
+    download_videos(video_location, youtube_url_file_location, ydl_opts)
+
+def get_youtube_dl_options(short_or_long, video_location):
+    # Return the download options for YouTube-DL depending on quality requirements
+
+    short_low_quality = {
+            'restrictfilenames': True,
+            'nopart': True,
+            'ignoreerrors': True,
+            'format': '133/134',
+            'outtmpl': video_location + '%(autonumber)s.%(ext)s'
+    }
+    
+    long_high_quality = {
+            'restrictfilenames': True,
+            'nopart': True,
+            'ignoreerrors': True,
+            'min_filesize': 400000000,
+            'format': '136/best[height=720]',
+            'outtmpl': video_location + '%(autonumber)s.%(ext)s'
+    }
+
+    if short_or_long == "short":
+        return short_low_quality
+
+    if short_or_long == "long":
+        return long_high_quality
+
+
+def download_videos(video_location, youtube_url_file_location, ydl_opts):
+    youtube_urls = [line.rstrip('\n') for line in open(youtube_url_file_location)]
+
+
+    # Download the files to the specified location
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download(youtube_urls)
+
+if __name__ == "__main__":
+    main()
